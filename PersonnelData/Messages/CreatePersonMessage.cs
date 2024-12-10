e1fd4d2bad37c67ed7b36455b750daa91243a64b
Konstantine Kargareteli <kote.kargareteli@gmail.com>
2024-12-10 15:15:25 +0400

    Remove required property access added suppress warning

--- PersonnelData/Messages/CreatePersonMessage.cs
+++ PersonnelData/Messages/CreatePersonMessage.cs
@@ -6,29 +6,31 @@ using PersonnelData.Shared.Enums;
 
 namespace PersonnelData.Messages;
 
+#pragma warning disable CS8618
+// Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 public class CreatePersonRequest
 {
     [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(SharedResources))]
     [StringLength(50, MinimumLength = 2, ErrorMessageResourceName = "StringLength", ErrorMessageResourceType = typeof(SharedResources))]
     [GeoLatinRestriction(ErrorMessageResourceName = "GeoLatinRestriction", ErrorMessageResourceType = typeof(SharedResources))]
-    public required string Name { get; set; }
-    
+    public string Name { get; set; }
+
     [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(SharedResources))]
     [StringLength(50, MinimumLength = 2, ErrorMessageResourceName = "StringLength", ErrorMessageResourceType = typeof(SharedResources))]
     [GeoLatinRestriction(ErrorMessageResourceName = "GeoLatinRestriction", ErrorMessageResourceType = typeof(SharedResources))]
-    public required string Surname { get; set; }
+    public string Surname { get; set; }
     
-    public required Gender Gender { get; set; }
+    public Gender Gender { get; set; }
     
     [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(SharedResources))]
     [StringLength(11, MinimumLength = 11, ErrorMessageResourceName = "ExactStringLength", ErrorMessageResourceType = typeof(SharedResources))]
-    public required string IdentificationNumber { get; set; }
+    public string IdentificationNumber { get; set; }
     
     [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(SharedResources))]
     [MinimumAge(18, ErrorMessageResourceName = "MinimumAge", ErrorMessageResourceType = typeof(SharedResources))]
-    public required DateTime BirthDate { get; set; }
+    public DateTime BirthDate { get; set; }
     
-    public required int CityId { get; set; }
+    public int CityId { get; set; }
     
-    public required List<PhoneDto> Phones { get; set; }
+    public List<PhoneDto> Phones { get; set; }
 }
